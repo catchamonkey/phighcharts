cb669ec5904210e12066bc6eef0c307d57219963
catchamonkey <chris@sedlmayr.co.uk>
2011-08-11 22:23:05 +0100

    Added the Defaults and Custom Options classes

---
+++ classes/Phighchart/Options/Defaults.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Phighchart\Options;
+use Phighchart\Options\Custom;
+
+/**
+ * Defines default Phighchart Options
+ * @author Chris Sedlmayr catchamonkey <chris@sedlmayr.co.uk>
+ */
+class Defaults extends Custom
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // setup the custom instance with our defaults
+    }
+}
