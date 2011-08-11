ea25ff91c6b2284796478649f31ecb577de69ec8
catchamonkey <chris@sedlmayr.co.uk>
2011-08-11 22:00:24 +0100

    Added more usage to the README Added more to the Chart class, including error checking and doblocks

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-#phighcharts
-
+#Phighcharts
 A PHP library for the Highcharts JavaScript charting library
+
+##What does it do?
+The library extends the functionality by adding such useful tools as
+
+###Adding a colour to be used by a certain key of data
+This will ensure that the same colour is used consistently for data with the same key
+
+    use Phighchart\Chart;
+    use Phighchart\Options\Defaults;
+    use Phighchart\Data\Data;
+    use Phighchart\Renderer\Pie;
+
+    $options    = new Defaults();
+    $options->addStickyColour('seo', '#8f8f8f');
+    $options->addStickyColour('ppc', '#3c3c3c');
+
+    $data       = new Data;
+    $data->addDataTotal('seo', 200);
+    $data->addDataTotal('ppc', 150);
+
+    $chart      = new Chart();
+    $chart->setOptions($options);
+    $chart->setData($data);
+    $chart->setRenderer('Pie');
+
+    // in the template
+    $chart->render();
--- classes/Phighchart/Chart.php
+++ classes/Phighchart/Chart.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Phighchart;
-use Phighchart\Options\Custom;
-use Phighchart\Data\Data;
+use Phighchart\Options\Custom as Options;
+use Phighchart\Data;
 use Phighchart\Renderer\Base as Renderer;
 
 /**
@@ -22,27 +22,48 @@ class Chart
         $this->_data        = FALSE;
     }
 
-    public function setOptions(Custom $options)
+    /**
+     * The options to use when rendering this chart
+     * @param Phighchart\Options\Custom $options stocked instance of chart options
+     */
+    public function setOptions(Options $options)
     {
         $this->_options = $options;
     }
 
+    /**
+     * The Data to use when rendering this chart
+     * @param Phighchart\Data $data stocked instance of chart data
+     */
     public function setData(Data $data)
     {
         $this->_data = $data;
     }
 
+    /**
+     * The Renderer to use when rendering this chart
+     * @param Phighchart\Renderer\Base $renderer Renderer class that implements
+     * the Phighchart\Renderer\Interface
+     */
     public function setRenderer(Renderer $renderer)
     {
         if (!class_exists($renderer))
         {
-            throw new Exception("Renderer class does not exist", 1);
+            throw new InvalidArgumentException("Renderer class does not exist", 1);
         }
         $this->_renderer = $renderer;
     }
 
+    /**
+     * Outputs the current chart instance
+     */
     public function render()
     {
+        // before we render we must have options and data
+        if (!this->_options || !$this->_data)
+        {
+            throw new Exception("Before rendering you must provide data and options", 1);
+        }
         return $this->_renderer->render($this);
     }
 }
