9de64043b97c05d52d290dc3e256aeff1a743096
catchamonkey <chris@sedlmayr.co.uk>
2011-08-11 22:22:47 +0100

    Added the Data class

---
+++ classes/Phighchart/Data.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Phighchart;
+
+/**
+ * Holds the data for a chart instance
+ * @author Chris Sedlmayr catchamonkey <chris@sedlmayr.co.uk>
+ */
+class Data
+{
+    private $_data;
+
+    public function __construct()
+    {
+        $this->_data = array();
+    }
+}
