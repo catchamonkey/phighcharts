16b60f67187e7ea0913fd214b3e4226a3734c357
shahrukhomar <shahrukh.omar@infinity-tracking.com>
2012-06-22 11:32:35 +0000

    Updated the unit tests for the line renderer to test for the cases when the extended options are not set

--- tests/unit/Renderer/LineTest.php
+++ tests/unit/Renderer/LineTest.php
@@ -61,4 +61,42 @@ class LineTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertSame('<div id="chart_example_59"></div>', $chart->renderContainer());
     }
+
+    public function testRenderWithoutExtendedOptions()
+    {
+        $options = new OptionsContainer('chart');
+        $options->setRenderTo('chart_example_59');
+        $options->setMarginRight(130);
+        $options->setMarginBottom(25);
+
+        $titleOptions = new OptionsContainer('title');
+        $titleOptions->setText('Monthly Details');
+        $titleOptions->setX(-20);
+
+        $data = new Data();
+        $data->addSeries('Apples', array(
+            '2012-05-01' => 12,
+            '2012-05-02' => 3,
+            '2012-05-03' => 33
+        ))
+        ->addSeries('Oranges', array(
+            '2012-05-01' => 32,
+            '2012-05-02' => 36,
+            '2012-05-03' => 18
+        ));
+
+        // put it all together
+        $chart  = new Chart();
+        $chart->addOptions($options);
+        $chart->addOptions($titleOptions);
+        $chart->setData($data);
+        $chart->setRenderer(new Line());
+
+        // test the full expected output
+        $this->assertSame(
+            'var chart_example_59; chart_example_59 = new Highcharts.Chart({"chart":{"renderTo":"chart_example_59","marginRight":130,"marginBottom":25,"type":"line"},"title":{"text":"Monthly Details","x":-20},"xAxis":{"categories":["2012-05-01","2012-05-02","2012-05-03"]},"series":[{"name":"Apples","data":[12,3,33]},{"name":"Oranges","data":[32,36,18]}]});',
+            $chart->render()
+        );
+        $this->assertSame('<div id="chart_example_59"></div>', $chart->renderContainer());
+    }
 }
