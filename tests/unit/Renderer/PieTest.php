ef293615493eb4d626057e0ccb33b7aa66bcb67f
shahrukhomar <shahrukh.omar@infinity-tracking.com>
2012-06-22 11:35:44 +0000

    Updated the unit tests for the pie renderer to test cases when the extended options are not set

--- tests/unit/Renderer/PieTest.php
+++ tests/unit/Renderer/PieTest.php
@@ -53,4 +53,34 @@ class PieTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertSame('<div id="chart_example_59"></div>', $chart->renderContainer());
     }
+
+    public function testRenderWithoutExtendedOptions()
+    {
+        $options = new OptionsContainer('chart');
+        $options->setRenderTo('chart_example_59');
+        $options->setMarginRight(130);
+        $options->setMarginBottom(25);
+
+        $titleOptions = new OptionsContainer('title');
+        $titleOptions->setText('Monthly Details');
+        $titleOptions->setX(-20);
+
+        $data = new Data();
+        $data->addCount('Apples', 32);
+        $data->addCount('Oranges', 68);
+
+        // put it all together
+        $chart  = new Chart();
+        $chart->addOptions($options);
+        $chart->addOptions($titleOptions);
+        $chart->setData($data);
+        $chart->setRenderer(new Pie());
+
+        // test the full expected output
+        $this->assertSame(
+            'var chart_example_59; chart_example_59 = new Highcharts.Chart({"chart":{"renderTo":"chart_example_59","marginRight":130,"marginBottom":25},"title":{"text":"Monthly Details","x":-20},"series":[{"type":"pie","data":[{"name":"Apples","y":32},{"name":"Oranges","y":68}]}]});',
+            $chart->render()
+        );
+        $this->assertSame('<div id="chart_example_59"></div>', $chart->renderContainer());
+    }
 }
