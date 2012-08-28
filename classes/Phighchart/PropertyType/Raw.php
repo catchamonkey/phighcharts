0883bb7ed594cfd7ac10d936ec6624a19602feec
shahrukhomar <shahrukh.omar@infinity-tracking.com>
2012-08-28 14:36:14 +0000

    Added raw property type so anything wrapped within this type is rendered as is during output

---
+++ classes/Phighchart/PropertyType/Raw.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PhighChart\PropertyType;
+
+/**
+ * Use the Type/Raw to mark the highchart properties that need to be rendered
+ * as they are and not to be rendered as string e.g. function callbacks,
+ * JS object types etc.
+ * @author Shahrukh Omar <shahrukhomar@gmail.com>
+ */
+
+class Raw
+{
+    const TYPE_RAW_DELIMITER = "~~";
+
+    /**
+     * Adds the raw delimiter to the given subject string
+     * @param  String $subject string to be rendered as raw
+     * @return String
+     */
+    public static function encode($subject)
+    {
+        return self::TYPE_RAW_DELIMITER.$subject.self::TYPE_RAW_DELIMITER;
+    }
+
+    /**
+     * Removes the raw delimiter from the given subject string
+     * @param  String $subject string to be decoded
+     * @return String
+     */
+    public static function decode($subject)
+    {
+        $patterns = array(
+            "/\"".self::TYPE_RAW_DELIMITER."/",
+            "/".self::TYPE_RAW_DELIMITER."\"/",
+            "/".self::TYPE_RAW_DELIMITER."/"
+        );
+
+        return preg_replace($patterns, '', $subject);
+    }
+}
